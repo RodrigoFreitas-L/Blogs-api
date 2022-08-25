8bd2beccf9526f89c1f43134642b2cee550b768e
RodrigoFreitas-L <kdigo.chr@gmail.com>
2022-08-25 15:52:25 -0300

    Implementado model User

---
+++ src/database/models/user.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    image: {
+      allowNull: true,
+      type: DataTypes.STRING,
+    },
+  }, { timestamps: false });
+
+  return User;
+};
